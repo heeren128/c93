134bc69581805f75dc14c46bdda6ebc9bcd02dfc
heeren128 <127753230+heeren128@users.noreply.github.com>
2023-06-21 17:36:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var canvas = new fabric.Canvas('myCanvas');
 
 player_x = 10;
@@ -13,10 +14,15 @@ function player_update()
 {
 	fabric.Image.fromURL("player.png", function(Img) {
 	player_object = Img;
+
 	player_object.scaleToWidth(150);
 	player_object.scaleToHeight(140);
-	player_object.set({top:player_y,left:player_x});
+	player_object.set({
+	top:player_y,
+	left:player_x
+	});
 	canvas.add(player_object);
+
 	});
 }
 
@@ -24,43 +30,51 @@ function new_image(get_image)
 {
 	fabric.Image.fromURL(get_image, function(Img) {
 	block_image_object = Img;
+
 	block_image_object.scaleToWidth(block_image_width);
 	block_image_object.scaleToHeight(block_image_height);
-	block_image_object.set({top:player_y,left:player_x});
+	block_image_object.set({
+	top:player_y,
+	left:player_x
+	});
 	canvas.add(block_image_object);
+
 	});
 
 }
 
-// Add an event listener and call the required function
 window.addEventListener("keydown", my_keydown);
 
-// Define the my_keydown function
-function my_keydown(e) {
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-	if(e.shiftKey == true && keyPressed == '80')
-	{
-		console.log("P and shift key pressed together")
-		block_image_width = block_image_width + 5;
-		block_image_height = block_image_height + 5;
-		document.getElementById("current_width").innerHTML = block_image_width;
-		document.getElementById("current_height").innerHTML = block_image_height;
-	}
-
-	if(e.shiftKey == true && keyPressed == '77')
-	{
-		console.log("M and shift key pressed together")
-		block_image_width = block_image_width - 5;
-		block_image_height = block_image_height - 5;
-		document.getElementById("current_width").innerHTML = block_image_width;
-		document.getElementById("current_height").innerHTML = block_image_height;
-	}
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+if(e.shiftKey == true && keyPressed == '80')
+{
+	console.log("p and shift pressed together");
+	block_image_width = block_image_width + 10;
+	block_image_height = block_image_height + 10;
+	document.getElementById("current_width").innerHTML = block_image_width;
+	document.getElementById("current_height").innerHTML = block_image_height;	
+}
+if(e.shiftKey && keyPressed == '77')
+{
+	console.log("m and shift pressed together");
+	block_image_width = block_image_width - 10;
+	block_image_height = block_image_height - 10;
+	document.getElementById("current_width").innerHTML = block_image_width;
+	document.getElementById("current_height").innerHTML = block_image_height;
+}
 
 	if(keyPressed == '38')
 	{
 		up();
 		console.log("up");
+	}
+	if(keyPressed == '40')
+	{
+		down();
+		console.log("down");
 	}
 	if(keyPressed == '37')
 	{
@@ -72,190 +86,99 @@ function my_keydown(e) {
 		right();
 		console.log("right");
 	}
-	if(keyPressed == '40')
-	{
-		down();
-		console.log("down");
-	}
-
 	if(keyPressed == '87')
 	{
-		new_image("wall.jpg")
+		new_image('wall.jpg'); 
 		console.log("w");
 	}
 	if(keyPressed == '71')
 	{
-		new_image("ground.png")
+		new_image('ground.png'); 
 		console.log("g");
 	}
 	if(keyPressed == '76')
 	{
-		new_image("light_green.png")
+		new_image('light_green.png'); 
 		console.log("l");
 	}
 	if(keyPressed == '84')
 	{
-		new_image("trunk.jpg")
+		new_image('trunk.jpg'); 
 		console.log("t");
 	}
 	if(keyPressed == '82')
 	{
-		new_image("roof.jpg")
+		new_image('roof.jpg'); 
 		console.log("r");
 	}
 	if(keyPressed == '89')
 	{
-		new_image("yellow_wall.png")
+		new_image('yellow_wall.png'); 
 		console.log("y");
 	}
 	if(keyPressed == '68')
 	{
-		new_image("dark_green.png")
+		new_image('dark_green.png'); 
 		console.log("d");
 	}
+	if(keyPressed == '85')
+	{
+		new_image('unique.png'); 
+		console.log("u");
+	}
+	if(keyPressed == '67')
+	{
+		new_image('cloud.jpg'); 
+		console.log("c");
+	}
+	
 }
-
-
-
-// Check if the SHIFT and P keys are pressed together
-
-
-
-
-
-
-
-
-// Check if the SHIFT and M keys are pressed together
-
-
-
-
-
-
-
-
-    // Check if the key value is equal to the UP arrow key value 38
-    
-	
-
-
-
-	// Check if the key value is equal to the DOWN arrow key value 40
-	
-	
-
-
-
-	// Check if the key value is equal to the LEFT arrow key value 37
-	
-
-
-
-
-	// Check if the key value is equal to the RIGHT arrow key value 39
-	
-
-
-
-
-	// Check if the key value is equal to the 'W' key value 87
-	
-
-
-
-
-	// Check if the key value is equal to the 'G' key value 71
-	
-
-
-
-
-	// Check if the key value is equal to the 'L' key value 76
-	
-
-
-
-
-	// Check if the key value is equal to the 'T' key value 84
-	
-
-
-
-
-	// Check if the key value is equal to the 'R' key value 82
-	
-
-
-
-
-	// Check if the key value is equal to the 'Y' key value 89
-	
-
-
-
-
-	// Check if the key value is equal to the 'D' key value 68
-	
-
-
-
-
-	// Check if the key value is equal to the 'U' key value 85
-	
-
-
-
-
-	// Check if the key value is equal to the 'C' key value 67
-	
-
-
-
-
-// Function to move the player up when UP arrow key is pressed
+//Complete the code for the the "up" function
 function up()
 {
-	if(player_y >=0)
+	if(player_y >=     )
 	{
-		player_y = player_y - block_image_height;
-		console.log("block image height = " + block_image_height);
+		player_y =                     ;
+		console.log("block image height = " +               );
 		console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
-		canvas.remove(player_object);
+		canvas.remove(              );
 		player_update();
 	}
 }
-// Function to move the player down when DOWN arrow key is pressed
+//Complete the code for the the "down" function
+
 function down()
 {
-	if(player_y <=500)
+	if(player_y <=     )
 	{
-		player_y = player_y + block_image_height;
-		console.log("block image height = " + block_image_height);
+		player_y =                      ;
+		console.log("block image height = " +                 );
 		console.log("When Down arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
 		canvas.remove(player_object);
 		player_update();
 	}
 }
-// Function to move the player left when LEFT arrow key is pressed
+//Complete the code for the the "left" fucntion
+
 function left()
 {
-	if(player_x >0)
+	if(player_x >     )
 	{
-		player_x = player_x - block_image_width;
-		console.log("block image width = " + block_image_width);
+		player_x =                     ;
+		console.log("block image width = " +                );
 		console.log("When Left arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
 		canvas.remove(player_object);
 		player_update();
 	}
 }
-// Function to move the player right when RIGHT arrow key is pressed
+//Complete the code for the the "right" fucntion
+
 function right()
 {
-	if(player_x <=850)
+	if(player_x <=     )
 	{
-		player_x = player_x + block_image_width;
-		console.log("block image width = " + block_image_width);
+		player_x =                       ;
+		console.log("block image width = "                     );
 		console.log("When Right arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
 		canvas.remove(player_object);
 		player_update();
